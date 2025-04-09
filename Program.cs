8ecfd6d5cbcccabd33b26f1b9edd6b155ae4c929
Anthony <mordo9027@gmail.com>
2025-04-09 10:16:43 -0500

    segundo commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.Write("Bievenido al curso de C#");
+            Console.Write("Bievenido al curso de C#");
             Console.ReadKey();
         }
     }
